fd42cf3f5a0d0bc207951eeb7b26f54fa315808f
Dylan Hand <dylan.hand@gmail.com>
2021-11-01 19:22:59 +0100

    It's ugly but it works happy halloween

--- assets/scripts/generate-thumbnails.sh
+++ assets/scripts/generate-thumbnails.sh
@@ -7,30 +7,20 @@ cd $parent_path # go to the scripts folder
 cd ../img
 
 thumbs_path="./thumbs"
-originals_path="./originals"
+optimized_path="./_optimized"
 
-# create the same directory structure in the thumbs folder
-function create_thumbnail_folder_structure() {
-  for subdirectory in $(find . -not -path "$thumbs_path/*" -type d)
+# creates the same directory structure in the given folder
+function create_folder_structure() {
+  for subdirectory in $(find . -not \( -path "$thumbs_path" -prune \) -not \( -path "$optimized_path" -prune \) -type d)
   do
     if [ $subdirectory != "." ]; then
-      mkdir -p $thumbs_path/$subdirectory
-    fi
-  done
-}
-
-# make the same directory structure for original pics
-function make_originals_folder_structure() {
-  for subdirectory in $(find . -not -path "$originals_path/*" -not \( -path "$thumbs_path" -prune \) -type d)
-  do
-    if [ $subdirectory != "." ]; then
-      mkdir -p originals/$subdirectory
+      mkdir -p $1/$subdirectory
     fi
   done
 }
 
 function make_thumbnails() {
-  for file in $(find . -not -path "$thumbs_path/*" -type f -iname "*[.jpg, .png]")
+  for file in $(find . -not \( -path "$optimized_path" -prune \) -not \( -path "$thumbs_path" -prune \) -type f -iname "*[.jpg, .png]")
   do
     # jpg thumbnails
     filename_jpg=$(echo "$file" | cut -c 3-) # cut off the "./"
@@ -49,15 +39,13 @@ function make_thumbnails() {
 }
 
 function optimize_images() {
-  for file in $(find . -not -path "$originals_path/*" -not -path "$thumbs_path/*" -type f -iname "*[.jpg, .png]")
+  for file in $(find . -not \( -path "$optimized_path" -prune \) -not \( -path "$thumbs_path" -prune \) -type f -iname "*[.jpg, .png]")
   do
     # jpg thumbnails
     filename_jpg=$(echo "$file" | cut -c 3-) # cut off the "./"
-    if [ ! -f "$originals_path/$filename_jpg" ]; then
+    if [ ! -f "$optimized_path/$filename_jpg" ]; then
       echo "Optimizing: $file"
-      # echo $file $originals_path
-      # mv $file $originals_path
-      # convert "$originals_path/$filename_jpg" -resize "1920>" -quality 85 "$filename_jpg"
+      convert "$filename_jpg" -resize "1920>" -quality 85 "$optimized_path/$filename_jpg"
     fi
 
     # webp thumbnails
@@ -69,7 +57,7 @@ function optimize_images() {
   done
 }
 
-create_thumbnail_folder_structure
+create_folder_structure $thumbs_path
+create_folder_structure $optimized_path
 make_thumbnails
-make_originals_folder_structure
 optimize_images
