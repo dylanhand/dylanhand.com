4a5341c0431b624f119e54caf91f1cabce0a30c3
Dylan Hand <dylan.hand@gmail.com>
2023-06-09 17:33:28 +0200

    Is this the problem

--- _includes/google-analytics.js
+++ _includes/google-analytics.js
@@ -2,5 +2,5 @@ loadScriptAsync('https://www.googletagmanager.com/gtag/js?id={{ site.google-anal
   window.dataLayer = window.dataLayer || [];
   function gtag() { dataLayer.push(arguments); }
   gtag('js', new Date());
-  gtag('config', '{{ site.google-analytics }}', { 'anonymize_ip': true });
+  gtag('config', '{{ site.google-analytics }}');
 })
